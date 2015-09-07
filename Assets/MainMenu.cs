492069071effb97c03d847a34cc9c3b97fe0b02a
C-Syratt <c.syratt@aanet.com.au>
2015-09-07 15:35:30 +1000

    Started Main Menu
    Some stuff with things

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public void StartGame()
+	{
+		Application.LoadLevelAsync("Game");
+	}
+
+}
