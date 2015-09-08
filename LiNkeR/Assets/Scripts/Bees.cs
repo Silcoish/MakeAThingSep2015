2694cb29e5b166c983bbebcf4850c4c962a4e1d3
C-Syratt <c.syratt@aanet.com.au>
2015-09-08 15:21:48 +1000

    Missiles + Start of Bees
    Plus some empty scripts for the some more items.
    Missiles, Fly and blow up on players and walls. No damage dealt yet.

---
+++ LiNkeR/Assets/Scripts/Bees.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bees : Item {
+
+	Vehicle hitPlayer;
+	bool playerHit = false;
+	float counter;
+
+	void Update () 
+	{
+		if(playerHit)
+		{
+			counter += Time.deltaTime;
+			transform.position = hitPlayer.transform.position;
+			if(counter > 4)
+			{
+				DestroyObject(gameObject);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			hitPlayer = col.gameObject.GetComponent<Vehicle>();
+			playerHit = true;
+		}
+	}
+
+}
---
+++ LiNkeR/Assets/Scripts/Missile.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : Item 
+{
+	public float speed;
+	public float damage;
+	[SerializeField] GameObject explosion;
+	[SerializeField] GameObject trailEffect;
+	Rigidbody2D rigid;
+	bool hitSomething = false;
+	float counter;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if(!hitSomething)
+		{
+			rigid.AddForce(transform.right * speed);
+		}
+		else
+		{
+			counter += Time.deltaTime;
+			if(counter > 0.5)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Wall")
+		{
+			gameObject.GetComponent<SpriteRenderer>().enabled = false;
+			trailEffect.gameObject.SetActive(false);
+			rigid.isKinematic = true;
+			explosion.SetActive(true);
+			hitSomething = true;
+		}
+		else if(col.gameObject.tag == "Player")
+		{
+			//col.gameObject.GetComponent<Vehicle>().linkedCar.SendMessage("Take Damage", damage);
+			Debug.Log("Do " + damage + " Damage to player" + col.gameObject.GetComponent<Vehicle>().playerID);
+			gameObject.GetComponent<SpriteRenderer>().enabled = false;
+			trailEffect.gameObject.SetActive(false);
+			rigid.isKinematic = true;
+			explosion.SetActive(true);
+			hitSomething = true;
+		}
+	}
+}
