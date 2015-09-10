34dc81338e0af1eab01c8f71c2dab711d263ca22
Silcoish <silcoish@gmail.com>
2015-09-10 14:34:52 +1000

    Magnet

---
+++ LiNkeR/Assets/Magnet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magnet : Item {
+
+    public float pullforce;
+    public float timer = 4f;
+    float counter = 0f;
+
+    public void Start()
+    {
+        transform.parent = owner.GetComponent<Vehicle>().itemInstPoint.transform;
+    }
+
+    public void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > timer)
+            Destroy(gameObject);
+        for(int i = 0; i < 4; i++)
+        {
+            GameObject tempCar = GameManager.inst.carsParent.transform.GetChild(i).gameObject;
+            if(tempCar != owner)
+            {
+                Vector2 normalisedDistance = ((Vector2)transform.position - (Vector2)tempCar.transform.position).normalized;
+                tempCar.GetComponent<Rigidbody2D>().AddForce(normalisedDistance * pullforce * Time.deltaTime);
+            }
+        }
+    }
+}
