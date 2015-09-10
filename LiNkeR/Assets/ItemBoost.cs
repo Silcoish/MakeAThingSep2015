9a9e47643cf88f978d8ccf30b3b7f6f56d43a67e
Silcoish <silcoish@gmail.com>
2015-09-10 11:22:09 +1000

    Health and Boost items and item images

---
+++ LiNkeR/Assets/ItemBoost.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemBoost : Item {
+
+    public float boostModifier = 1.0f;
+    public float boostTime = 3.0f;
+	void Start()
+    {
+        owner.GetComponent<Vehicle>().SetBoostSpeed(boostModifier, boostTime);
+        Destroy(gameObject);
+    }
+}
