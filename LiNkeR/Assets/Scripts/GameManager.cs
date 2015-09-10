da0d71c8404eecb7ee3ab1856ab4a578b0b45253
C-Syratt <c.syratt@aanet.com.au>
2015-09-10 17:03:58 +1000

    GUI Lap Text

--- LiNkeR/Assets/Scripts/GameManager.cs
+++ LiNkeR/Assets/Scripts/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour {
 	Vehicle[] racePositions = new Vehicle[4];
     public Image[] itemIconLocations = new Image[4];
 	public Image[] characterIconLocations = new Image[4];
+	public Text[] characterLapLocations = new Text[4];
     public Sprite defaultItemIcon;
 
 	public List<GameObject> itemList;
@@ -46,6 +47,7 @@ public class GameManager : MonoBehaviour {
                         CharacterManager cm = GameObject.Find("CharacterManager").GetComponent<CharacterManager>();
                         carsParent.transform.GetChild(i).GetComponent<Vehicle>().playerCharacter = cm.characters[cm.characterID[i]].GetComponent<Character>();
 						SetCharacterIcons(i);
+						SetCharacterInfo(i);
                     }
 				}
 				set = true;
@@ -114,6 +116,47 @@ public class GameManager : MonoBehaviour {
 	public void SetCharacterIcons(int player)
 	{
 		characterIconLocations[player].sprite = carsParent.transform.GetChild(player).GetComponent<Vehicle>().playerCharacter.characterIcon;
+	}
+
+	public void SetCharacterInfo(int player)
+	{
+		Vehicle playerer = carsParent.transform.GetChild(player).GetComponent<Vehicle>();
+		print ("Setting the info");
+		switch(playerer.currentPosition)
+		{
+		case 1:
+			print ("pos 1");
+			characterLapLocations[player].text = (playerer.currentPosition) + "st";
+			break;
+		case 2:
+			print ("pos 2");
+			characterLapLocations[player].text = (playerer.currentPosition) + "nd";
+			break;
+		case 3:
+			print ("pos 3");
+			characterLapLocations[player].text = (playerer.currentPosition) + "rd";
+			break;
+		case 4:
+			print ("pos 4");
+			characterLapLocations[player].text = (playerer.currentPosition) + "th";
+			break;
+		}
+		switch(playerer.lap)
+		{
+		case 0:
+			characterLapLocations[player].text += characterLapLocations[player].text + "BLAH" + playerer.lap.ToString();// + "/3";
+			break;
+		case 1:
+			characterLapLocations[player].text += playerer.lap;// + "/3";
+			break;
+		case 2:
+			characterLapLocations[player].text += playerer.lap;// + "/3";
+			break;
+		case 3:
+			characterLapLocations[player].text += playerer.lap;// + "/3";
+			break;
+		}
+		print(characterLapLocations[player].text);
 	}
 
     public void SetDefaultIcon(int player)
